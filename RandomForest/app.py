6366ada84d59891f7a020ab0a0918534c77bd995
Joseph-Ordonez <joseph.ordonez@ucsm.edu.pe>
2024-10-17 08:23:31 -0500

    Random Forest: Pruebas y ajustes

--- RandomForest/app.py
+++ RandomForest/app.py
@@ -14,11 +14,45 @@ def Home():
 
 @app.route("/predict", methods = ["POST"])
 def predict():
-    float_features = [float(x) for x in request.form.values()]
-    features = [np.array(float_features)]
-    prediction = model.predict(features)
+    try:
+        float_features = [float(x) for x in request.form.values()]
+        features = [np.array(float_features)]
+        prediction = model.predict(features)
 
-    return render_template("index.html", prediction_text = "The recommendation is: {}".format(prediction))
+        return jsonify({
+                "prediction": prediction[0]
+            })
+    except Exception as e:
+        return jsonify({
+            "error": str(e)
+        }), 400
+
+    # return render_template("index.html", prediction_text = "The recommendation is: {}".format(prediction))
+
+
+# @app.route("/predict", methods=["POST"])
+# def predict():
+#     try:
+#         # Ver el JSON recibido
+#         print(request.get_json(force=True))
+#         data = request.get_json(force=True)
+#         float_features = [float(x) for x in data["features"]]
+        
+#         if len(float_features) != 87:
+#             return jsonify({"error": "Se requieren 87 características."}), 400
+        
+#         features = [np.array(float_features)]
+#         prediction = model.predict(features)
+        
+#         return jsonify({
+#             "prediction": prediction[0]
+#         })
+    
+#     except Exception as e:
+#         return jsonify({
+#             "error": str(e)
+#         }), 400
+
 
 if __name__ == "__main__":
     app.run(debug=True)
